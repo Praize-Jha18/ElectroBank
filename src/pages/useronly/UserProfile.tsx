73311d36ea90b30d1fe8ff09e4d3b22feceec0ae
ayindequdus22 <ayindeabdulqudus22@gmail.com>
2024-10-12 11:30:37 -0700

    deployment json file

--- src/pages/useronly/UserProfile.tsx
+++ src/pages/useronly/UserProfile.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 import { CheckmarkCircleOutline } from 'react-ionicons'
