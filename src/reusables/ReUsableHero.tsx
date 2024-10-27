87ba105bcb7a521da006d9f7fb9f8acada2b5283
ayindequdus22 <ayindeabdulqudus22@gmail.com>
2024-10-27 13:40:37 -0700

    added the typing animation

--- src/reusables/ReUsableHero.tsx
+++ src/reusables/ReUsableHero.tsx
@@ -1,3 +1,4 @@
+import { TypeAnimation } from 'react-type-animation';
 const ReUsableHero = (props: { img: string | null; heading: string; height: string | null }) => {
   const height = props.height ? props.height : '70vh';
   return (
@@ -13,8 +14,33 @@ const ReUsableHero = (props: { img: string | null; heading: string; height: stri
 
 
       </div>
-      <div className={`absolute pl-24 pr-6 max-tab:pl-20 max-mdPhone:pl-12 df-ac ${props.img  == null?  " bg-teal-800 " :"bg-neutral-800"}  bg-opacity-70 h-full w-full`}>
-        <h1 className="text-[3.125rem] leading-snug font-bold text-white font-numans w-[50%] max-mdLap:w-[70%] max-tab:w-[100%] max-tab:text-[2.5rem] max-mdPhone:text-[2rem] max-phone:text-[1.8rem]">{props.heading}</h1>
+      <div className={`absolute pl-24 pr-6 max-tab:pl-20 max-mdPhone:pl-12 df-ac ${props.img == null ? " bg-teal-800 " : "bg-neutral-800"}  bg-opacity-70 h-full w-full`}>
+        <h1 className="text-[3.125rem] leading-snug font-bold text-white font-numans w-[50%] max-mdLap:w-[70%] max-tab:w-[100%] max-tab:text-[2.5rem] max-mdPhone:text-[2rem] max-phone:text-[1.8rem]">
+
+        <TypeAnimation
+  sequence={[
+    // Same substring at the start will only be typed once, initially
+    props.heading,
+    5000,
+    '', 2000, 
+    props.heading 
+
+   
+  ]}
+  speed={50}
+
+  repeat={Infinity}
+/>
+{/* 
+          <TypeAnimation sequence={[   props.heading, 
+    5000,
+    '', 
+    2000, 
+    props.heading ]}
+            speed={75} repeat={Infinity}
+          /> */}
+          {/* {props.heading} */}
+        </h1>
       </div>
     </div>
   );
