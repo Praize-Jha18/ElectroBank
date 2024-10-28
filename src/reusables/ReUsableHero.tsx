d01f24f9c7571a822cb515b1ee57150e6a6d7367
ayindequdus22 <ayindeabdulqudus22@gmail.com>
2024-10-28 04:30:17 -0700

    Fixed smh

--- src/reusables/ReUsableHero.tsx
+++ src/reusables/ReUsableHero.tsx
@@ -17,21 +17,21 @@ const ReUsableHero = (props: { img: string | null; heading: string; height: stri
       <div className={`absolute pl-24 pr-6 max-tab:pl-20 max-mdPhone:pl-12 df-ac ${props.img == null ? " bg-teal-800 " : "bg-neutral-800"}  bg-opacity-70 h-full w-full`}>
         <h1 className="text-[3.125rem] leading-snug font-bold text-white font-numans w-[50%] max-mdLap:w-[70%] max-tab:w-[100%] max-tab:text-[2.5rem] max-mdPhone:text-[2rem] max-phone:text-[1.8rem]">
 
-        <TypeAnimation
-  sequence={[
-    // Same substring at the start will only be typed once, initially
-    props.heading,
-    5000,
-    '', 2000, 
-    props.heading 
+          <TypeAnimation
+            sequence={[
+              // Same substring at the start will only be typed once, initially
+              props.heading,
+              5000,
+              '', 2000,
+              props.heading
 
-   
-  ]}
-  speed={50}
 
-  repeat={Infinity}
-/>
-{/* 
+            ]}
+            speed={25}
+
+            repeat={Infinity}
+          />
+          {/* 
           <TypeAnimation sequence={[   props.heading, 
     5000,
     '', 
